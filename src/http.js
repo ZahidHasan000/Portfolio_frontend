b99ed409220e16a42331060296fe13f4b8f1c38b
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2025-12-03 02:28:16 +0600

    first commit

--- src/http.js
+++ src/http.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 export default axios.create({
   // baseURL: `${import.meta.env.REACT_APP_PRO_MODE_URL}`,
-  baseURL: `${'http://localhost:5000/api/'}`,
+  // baseURL: `${'http://localhost:5000/api/'}`,
+  baseURL: `${'https://zahid-portfolio-1.onrender.com/api/'}`,
   headers: {
     // "Content-Type": "application/json"
     'Content-Type': 'multipart/form-data'
