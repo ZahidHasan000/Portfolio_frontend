08a7ee04a251433f48b3f3201df1231615531402
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2025-12-11 02:28:13 +0600

    11dec

--- src/http.js
+++ src/http.js
@@ -5,7 +5,7 @@ export default axios.create({
   // baseURL: `${process.env.REACT_APP_GET_BASE_URL}`,
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    // "Content-Type": "application/json"
+    "Content-Type": "application/json",
     'Content-Type': 'multipart/form-data'
   }
 });
